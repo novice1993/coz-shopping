b35261a1374f04e2d76028c2093260526b078738
novice1993 <novice1993@gmail.com>
2023-08-05 23:01:17 +0900

    불필요한 주석 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,6 @@ import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 
-// 나머지 리팩토링 부분
-// 2) ts 적용 -> ok
-// 3) grid 레이아웃 적용
-// 코드 수정 몇 군데
-// ##) 마무리 -> 블로그 글 작성
-// 4) 리액트 쿼리 적용 -> no 
-
 function App() {
 
   return (

3f66673b54542e3700f4c21cb1fbf9e17cb04c80
novice1993 <novice1993@gmail.com>
2023-10-14 20:55:05 +0900

    [Fix] 오류 코드 수정
    - 일부 오류 발생하는 코드 수정
    
    Issues #13

--- src/reducer/Item-Reducer.ts
+++ src/reducer/Item-Reducer.ts
@@ -4,26 +4,28 @@ import ItemProps from "../models/ItemProps";
 const initialState: ItemProps[] = [];
 
 const itemSlice = createSlice({
-    name: 'itemList',
-    initialState: initialState,
-    reducers: {
-        addItemList: (state: ItemProps[], action: PayloadAction<ItemProps[]>): ItemProps[] => {
+  name: "itemList",
+  initialState: initialState,
+  reducers: {
+    addItemList: (state: ItemProps[], action: PayloadAction<ItemProps[]>) => {
+      if (action.payload !== undefined) {
+        const notOverlappingItem = action.payload.filter((item: ItemProps) => {
+          // 데이터 중복검사 시행
 
-            const notOverlappingItem = action.payload.filter((item: ItemProps) => { // 데이터 중복검사 시행
+          let result = 0;
 
-                let result = 0;
+          for (let i = 0; i < state.length; i++) {
+            state[i].id === item.id && (result += 1);
+          }
 
-                for(let i=0; i<state.length; i++){
-                    (state[i].id === item.id) && (result += 1)
-                }
-                
-                return (result === 0);
-            })
+          return result === 0;
+        });
 
-            return [...state, ...notOverlappingItem];
-        }
-    }
-})
+        return [...state, ...notOverlappingItem];
+      }
+    },
+  },
+});
 
 export const { addItemList } = itemSlice.actions;
 export const itemReducer = itemSlice.reducer;
